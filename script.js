a8768bad91031b33a1a7be0054552811f101de77
Sangkrishna <124071760+sangkrishna7@users.noreply.github.com>
2024-08-08 12:39:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -128,5 +128,16 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
                 });
             });
+           document.querySelectorAll('#footer-content a').forEach(link => {
+                link.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    const targetId = this.getAttribute('href').substring(1); 
+                    const targetElement = document.getElementById(targetId);
+                    if (targetElement) {
+                        targetElement.scrollIntoView({ behavior: 'smooth' });
+                   }
+               });
+           });
+
         });
 });
